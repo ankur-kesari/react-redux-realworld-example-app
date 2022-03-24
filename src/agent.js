1d6f719e7cc9fd4e32938f63ed079c199ebce2c4
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-03-24 12:19:55 +0530

    modified http to https in api root

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://ankur6.squareops.co.in/api';
+const API_ROOT = 'https://ankur6.squareops.co.in/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
