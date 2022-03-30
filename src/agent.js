fb862ed07ef7376de4325225f6a44c1e65d6ea98
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-03-30 11:38:00 +0530

    use env. var from amplify in api root of agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://ankur10.squareops.co.in/api';
+const API_ROOT = process.env.REACT_APP_API_ROOT;
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
