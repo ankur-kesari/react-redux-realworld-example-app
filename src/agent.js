a3e27d1050dd330713abeea7026b9c489eb56c98
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-08-03 16:26:27 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = process.env.REACT_APP_API_ROOT;
+const API_ROOT = process.env.REACT_APP_API_ROOT;         //define REACT_APP_API_ROOT = ALB-endpoint (in environment variable of aws amplify) 
 //const API_ROOT = 'https://conduit.productionready.io/api';
 
 const encode = encodeURIComponent;
